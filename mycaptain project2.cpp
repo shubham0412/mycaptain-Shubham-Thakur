a30120d183e546443f3bb32eb0e8120473c708c9
shubham0412 <67813968+shubham0412@users.noreply.github.com>
2020-07-05 13:57:09 +0530

    Add files via upload

---
+++ mycaptain project2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double a, b, product;
+	
+	cout << "Enter two numbers: ";
+	
+	cin >> a >> b;
+	
+	product = a*b;
+	
+	cout << "Product = " << product;
+	
+	return 0;
+	
+}
